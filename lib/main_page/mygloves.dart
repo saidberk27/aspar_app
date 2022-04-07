b4ecc52094a085ce96d360bc5aa83b5932920621
unknown <csaidberk@gmail.com>
2022-04-07 16:10:38 +0300

    Firestore Pagination (Sayfalama İşlemi Tamamlandı.)

--- lib/main_page/mygloves.dart
+++ lib/main_page/mygloves.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutterfire_ui/firestore.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:paginate_firestore/paginate_firestore.dart';
 
 class MyGloves extends StatefulWidget {
   MyGloves({Key? key}) : super(key: key);
@@ -18,20 +18,34 @@ class _MyGlovesState extends State<MyGloves> {
         .doc("1")
         .collection("ARC")
         .doc("CLASS 0")
-        .collection("TR013C0S08000A")
         .get();
-    var eldivenler = response.docs;
-    print(eldivenler.first.data());
+
+    return response.data();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: OutlinedButton(
-      child: Text("Get Data"),
-      onPressed: () {
-        _getGloves();
-      },
-    ));
+      body: PaginateFirestore(
+        // Use SliverAppBar in header to make it sticky
+        header: const SliverToBoxAdapter(child: Text('HEADER')),
+        footer: const SliverToBoxAdapter(child: Text('FOOTER')),
+        // item builder type is compulsory.
+        itemBuilderType:
+            PaginateBuilderType.listView, //Change types accordingly
+        itemBuilder: (context, documentSnapshots, index) {
+          final data = documentSnapshots[index].data() as Map?;
+          return ListTile(
+            leading: const CircleAvatar(child: Icon(Icons.person)),
+            title: data == null ? const Text('Error in data') : Text("Deneme"),
+            subtitle: Text(documentSnapshots[index].id),
+          );
+        },
+        // orderBy is compulsory to enable pagination
+        query: FirebaseFirestore.instance.collection('app_test'),
+        // to fetch real-time data
+        isLive: true,
+      ),
+    );
   }
 }
