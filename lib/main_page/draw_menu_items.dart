f31c5fc8022f80cf37284c17743e1c64d549d03f
unknown <csaidberk@gmail.com>
2022-05-11 16:27:01 +0300

    Drawer Menu Tamamen Yenilendi.

--- lib/main_page/draw_menu_items.dart
+++ lib/main_page/draw_menu_items.dart
@@ -10,134 +10,140 @@ class MenuItems extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      padding: EdgeInsets.zero,
-      children: [
-        const Divider(
-          thickness: 3,
-          color: Color(0xFF166FC0),
-          height: 3,
-        ),
-        InkWell(
-          onTap: () {
-            debugPrint("Tapped");
-            Navigator.of(context).push(
-                MaterialPageRoute(builder: (glovesContext) => MyGloves()));
-          },
-          child: ListTile(
-            leading: Image.asset(
-              "assets/images/gloves.png",
-              scale: 1.5,
-            ),
-            title: Text(
-              "Eldivenleriniz",
-              style: TextStyle(fontSize: 18),
-            ),
-            trailing: Icon(
-              Icons.navigate_next_sharp,
-              color: Color(0xFF365059),
-            ),
+    return Container(
+      decoration: BoxDecoration(
+          border: Border(
+        right: BorderSide(width: 3.0, color: Color(0xFF166FC0)),
+      )),
+      child: ListView(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        padding: EdgeInsets.zero,
+        children: [
+          const Divider(
+            thickness: 3,
+            color: Color(0xFF166FC0),
+            height: 3,
           ),
-        ),
-        const Divider(thickness: 3, color: Color(0xFF0FA9EA)),
-        InkWell(
-            child: ListTile(
-          onTap: () {},
-          leading: Image.asset(
-            "assets/images/new.png",
-            scale: 1.5,
-          ),
-          title: const Text("Aspar Enerji'de Yeni Ne Var?",
-              style: TextStyle(fontSize: 18)),
-          trailing: const Icon(
-            Icons.navigate_next_sharp,
-            color: Color(0xFF365059),
-          ),
-        )),
-        const Divider(thickness: 3, color: Color(0xFF166FC0)),
-        InkWell(
+          InkWell(
             onTap: () {
+              debugPrint("Tapped");
               Navigator.of(context).push(
-                  MaterialPageRoute(builder: (newsContext) => const Blog()));
+                  MaterialPageRoute(builder: (glovesContext) => MyGloves()));
             },
             child: ListTile(
               leading: Image.asset(
-                "assets/images/info.png",
+                "assets/images/gloves.png",
                 scale: 1.5,
               ),
               title: Text(
-                "Hakkımızda",
+                "Eldivenleriniz",
                 style: TextStyle(fontSize: 18),
               ),
               trailing: Icon(
                 Icons.navigate_next_sharp,
                 color: Color(0xFF365059),
               ),
-            )),
-        const Divider(thickness: 3, color: Color(0xFF0FA9EA)),
-        InkWell(
-          onTap: () {},
-          child: ListTile(
+            ),
+          ),
+          const Divider(thickness: 3, color: Color(0xFF0FA9EA)),
+          InkWell(
+              child: ListTile(
+            onTap: () {},
             leading: Image.asset(
-              "assets/images/help.png",
+              "assets/images/new.png",
               scale: 1.5,
             ),
-            title: Text(
-              "Yardım",
-              style: TextStyle(fontSize: 18),
-            ),
-            trailing: Icon(
+            title: const Text("Aspar Enerji'de Yeni Ne Var?",
+                style: TextStyle(fontSize: 18)),
+            trailing: const Icon(
               Icons.navigate_next_sharp,
               color: Color(0xFF365059),
             ),
-          ),
-        ),
-        const Divider(thickness: 3, color: Color(0xFF166FC0)),
-        InkWell(
-          onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(
-                builder: (contactContext) => const Contact()));
-          },
-          child: ListTile(
-            leading: Image.asset(
-              "assets/images/contact.png",
-              scale: 1.5,
-            ),
-            title: Text(
-              "İletişim",
-              style: TextStyle(fontSize: 18),
-            ),
-            trailing: Icon(
-              Icons.navigate_next_sharp,
-              color: Color(0xFF365059),
-            ),
-          ),
-        ),
-        const Divider(thickness: 3, color: Color(0xFF0FA9EA)),
-        InkWell(
-          onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(
-                builder: (contactContext) => const Contact()));
-          },
-          child: ListTile(
-            leading: Image.asset(
-              "assets/images/exit.png",
-              scale: 1.5,
-            ),
-            title: Text(
-              "Çıkış Yap",
-              style: TextStyle(fontSize: 18),
-            ),
-            trailing: Icon(
-              Icons.navigate_next_sharp,
-              color: Color(0xFF365059),
+          )),
+          const Divider(thickness: 3, color: Color(0xFF166FC0)),
+          InkWell(
+              onTap: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (newsContext) => const Blog()));
+              },
+              child: ListTile(
+                leading: Image.asset(
+                  "assets/images/info.png",
+                  scale: 1.5,
+                ),
+                title: Text(
+                  "Hakkımızda",
+                  style: TextStyle(fontSize: 18),
+                ),
+                trailing: Icon(
+                  Icons.navigate_next_sharp,
+                  color: Color(0xFF365059),
+                ),
+              )),
+          const Divider(thickness: 3, color: Color(0xFF0FA9EA)),
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              leading: Image.asset(
+                "assets/images/help.png",
+                scale: 1.5,
+              ),
+              title: Text(
+                "Yardım",
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: Icon(
+                Icons.navigate_next_sharp,
+                color: Color(0xFF365059),
+              ),
             ),
           ),
-        ),
-        const Divider(thickness: 3, color: Color(0xFF166FC0)),
-      ],
+          const Divider(thickness: 3, color: Color(0xFF166FC0)),
+          InkWell(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (contactContext) => const Contact()));
+            },
+            child: ListTile(
+              leading: Image.asset(
+                "assets/images/contact.png",
+                scale: 1.5,
+              ),
+              title: Text(
+                "İletişim",
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: Icon(
+                Icons.navigate_next_sharp,
+                color: Color(0xFF365059),
+              ),
+            ),
+          ),
+          const Divider(thickness: 3, color: Color(0xFF0FA9EA)),
+          InkWell(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (contactContext) => const Contact()));
+            },
+            child: ListTile(
+              leading: Image.asset(
+                "assets/images/exit.png",
+                scale: 1.5,
+              ),
+              title: Text(
+                "Çıkış Yap",
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: Icon(
+                Icons.navigate_next_sharp,
+                color: Color(0xFF365059),
+              ),
+            ),
+          ),
+          const Divider(thickness: 3, height: 3, color: Color(0xFF166FC0)),
+        ],
+      ),
     );
   }
 }
--- lib/main_page/drawer_user_information.dart
+++ lib/main_page/drawer_user_information.dart
@@ -9,21 +9,41 @@ class UserInformation extends StatelessWidget {
   Widget build(BuildContext context) {
     return UserAccountsDrawerHeader(
       margin: EdgeInsets.zero,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            top: BorderSide(width: 3.0, color: Color(0xFF166FC0)),
+            left: BorderSide(width: 3.0, color: Color(0xFF166FC0)),
+            right: BorderSide(width: 3.0, color: Color(0xFF166FC0)),
+            bottom: BorderSide(width: 0),
+          )),
       accountName: Text(
         "KULLANICI ADI",
         style: TextStyle(
-            fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF166FC0)),
       ),
       accountEmail: Text(
         "email@aspar.com",
         style: TextStyle(
-            fontSize: 14, fontWeight: FontWeight.bold, color: Colors.white60),
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF0FA9EA)),
       ),
       currentAccountPicture: CircleAvatar(
-        child: Text(
-          "AS",
-          style: TextStyle(
-              fontWeight: FontWeight.bold, fontSize: 40, color: Colors.black),
+        radius: 40,
+        backgroundColor: Color(0xFF0FA9EA),
+        child: CircleAvatar(
+          radius: 32,
+          backgroundColor: Colors.white,
+          child: Text(
+            "AS",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+                color: Color(0xFF166FC0)),
+          ),
         ),
       ),
     );
